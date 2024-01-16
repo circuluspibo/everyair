ba511024fbe1e00b8332110f4af5ef5ce67b6bbf
unknown <rippertnt@circul.us>
2024-01-16 17:57:35 +0900

    update

---
+++ audio.py
@@ -0,0 +1,18 @@
+
+def stt():
+  print("stt")
+
+def tts():
+  print("tts")
+
+def classify_sound():
+  print("classifiy sound")
+
+def classifiy_emotion():
+  print("classify emotion")
+
+def generate_sound():
+  print("generate sound")
+
+def generate_music():
+  print("generate music")  
